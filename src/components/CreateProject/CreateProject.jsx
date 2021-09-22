c7227d1dde1a99dc73c5bfbde4892aa7100a8137
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2021-09-22 16:52:29 +0300

    change create project form

--- src/components/CreateProject/CreateProject.jsx
+++ src/components/CreateProject/CreateProject.jsx
@@ -1,43 +1,53 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Input from '../Input/Input'
 import './style.scss'
-import {createProject} from '../../actions/user'
+import { createProject } from '../../actions/user'
+import { Formik } from 'formik'
+import { validationShema, initialValues } from '../../variables/variables'
 
-const CreateProject = ({closeEvent}) => {
-    const [title, setTitle] = useState("")
-    const [key, setKey] = useState("")
 
+const CreateProject = ({ closeEvent }) => {
 
     return (
-        <>
-            <div className="blur-box" onClick={closeEvent}>
-            </div>
-            <form className="create_form" onSubmit={(e)=> e.target.reset()}>
-                <p className="create_title">
-                    Create a new project
-                </p>
-                <div className="create_box row">
-                    <Input className={"create-title"} placeholder={"Title"} value={title} setValue={setTitle} />
-                    <Input className={"create-key"} placeholder={"Key"} value={key} setValue={setKey} />
-                </div>
-                <div className="create_box row end">
-                    <button
-                        onClick={closeEvent}
-                        className="register-login"
-                    >
-                        Cancel
-                    </button>
-                    <button
-                        type="submit"
-                        className="register-create"
-                        onClick = {()=>createProject(title, key)}
-                    >
-                        Create
-                    </button>
-                </div>
+        <Formik
+            initialValues={initialValues}
+            validateOnBlur // Validation when you come to another field
+            validationSchema={validationShema}
+            onSubmit={values => createProject(values)}
 
-            </form>
-        </>
+        >
+            {({ values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
+                <>
+                    <div className="blur-box" onClick={closeEvent}>
+                    </div>
+                    <form className="create_form" onSubmit={(e) => e.target.reset()}>
+                        <p className="create_title">
+                            Create a new project
+                        </p>
+                        <div className="create_box row">
+                            <Input className={"create-title"} placeholder={"Title"} value={title} setValue={setTitle} />
+                            <Input className={"create-key"} placeholder={"Key"} value={key} setValue={setKey} />
+                        </div>
+                        <div className="create_box row end">
+                            <button
+                                onClick={closeEvent}
+                                className="register-login"
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                type="submit"
+                                disabled={!isValid && !dirty}
+                                onClick={handleSubmit}
+                            >
+                                Create
+                            </button>
+                        </div>
+
+                    </form>
+                </>
+            )}
+        </Formik>
 
     )
 }
